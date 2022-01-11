8137752b69b935b277ed21440f025a2e12284e32
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-11 14:20:27 +0100

    Create HomeSearchBar.js

---
+++ src/components/home-page/HomeSearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeSearchBar = () => {
+    return (
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default HomeSearchBar;
