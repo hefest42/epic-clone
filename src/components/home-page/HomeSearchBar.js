1c7a6dbf2b65efb0c479529a846cff92d8c90f24
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-11 14:39:12 +0100

    basic layout

--- src/components/home-page/HomeSearchBar.js
+++ src/components/home-page/HomeSearchBar.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const HomeSearchBar = () => {
     return (
-        <div>
-            <div></div>
-            <div></div>
+        <div className="search-bar">
+            <div className="search-bar__left">
+                <form>
+                    <input type="text" />
+                </form>
+                <div>
+                    <p>Discover</p>
+                    <p>Browse</p>
+                    <p>News</p>
+                </div>
+            </div>
+
+            <div className="search-bar__right">
+                <p>Wishlist</p>
+                <p>Cart</p>
+            </div>
         </div>
     );
 };
