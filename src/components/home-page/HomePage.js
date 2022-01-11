05c536b5041a2bd3a46460d1c01c4d00afb17f0b
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-11 15:00:51 +0100

    added more nested routes

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -13,7 +13,10 @@ const HomePage = () => {
 
                 <Routes>
                     <Route path="discover" element={<p className="test">discover</p>} />
-                    <Route path="browse" element={<p className="test">browse</p>} />
+                    <Route path="browse" element={<p className="test"></p>} />
+                    <Route path="news" element={<p className="test"></p>} />
+                    <Route path="wishlist" element={<p className="test"></p>} />
+                    <Route path="cart" element={<p className="test"></p>} />
                 </Routes>
             </div>
         </div>
