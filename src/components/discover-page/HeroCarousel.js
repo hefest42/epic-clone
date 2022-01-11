4492118e8f176c56c62069f37ce85cc6aec64007
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-11 15:52:15 +0100

    basic layout

---
+++ src/components/discover-page/HeroCarousel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroCarousel = () => {
+    return (
+        <div className="hero-container">
+            <div className="hero-left"></div>
+            <div className="hero-right"></div>
+        </div>
+    );
+};
+
+export default HeroCarousel;
