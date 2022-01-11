d9345bc80da9d0f0a36aa85def15b8c2a9b8b2a4
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-11 14:39:01 +0100

    added a wrapping div

--- src/components/home-page/HomePage.js
+++ src/components/home-page/HomePage.js
@@ -2,16 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "../header/Header";
+import HomeSearchBar from "./HomeSearchBar";
 
 const HomePage = () => {
     return (
         <div>
             <Header />
+            <div className="home-container">
+                <HomeSearchBar />
 
-            <Routes>
-                <Route path="discover" element={<p className="test">discover</p>} />
-                <Route path="browse" element={<p className="test">browse</p>} />
-            </Routes>
+                <Routes>
+                    <Route path="discover" element={<p className="test">discover</p>} />
+                    <Route path="browse" element={<p className="test">browse</p>} />
+                </Routes>
+            </div>
         </div>
     );
 };
