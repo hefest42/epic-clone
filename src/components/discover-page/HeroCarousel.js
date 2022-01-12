d374d8bd00f125b9a75a006f8be8e8dc5d64ad61
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-12 16:25:55 +0100

    basic slider layout(not completed)

--- src/components/discover-page/HeroCarousel.js
+++ src/components/discover-page/HeroCarousel.js
@@ -1,18 +1,41 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
 
 import { HERO_GAMES } from "../../dummy-server/DUMMY_GAMES";
 
 const HeroCarousel = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const nextSlideHandler = () => {
+        setCurrentSlide(state => state + 1);
+    };
+
+    const prevSlideHandler = () => {
+        setCurrentSlide(state => state - 1);
+    };
+
     return (
         <div className="hero-container">
             <div className="hero-left">
                 {HERO_GAMES.map((game, i) => {
                     return (
                         <div className="poster-container">
-                            <img src={game.posterBig} alt="" />
+                            <img
+                                key={i}
+                                style={{
+                                    transform: `translateX(${(i - currentSlide) * 100}%)`,
+                                }}
+                                src={game.posterBig}
+                                alt=""
+                            />
                         </div>
                     );
                 })}
+
+                <div className="button-container">
+                    <button onClick={prevSlideHandler}>LEFT</button>
+                    <button onClick={nextSlideHandler}>RIGHT</button>
+                </div>
             </div>
             <div className="hero-right"></div>
         </div>
