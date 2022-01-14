0611ef8cac76eb6305b378c7aef813af85d1143f
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-14 16:34:36 +0100

    implemented img in the main carousel

--- src/components/sub-pages/discover-components/MainCarousel.js
+++ src/components/sub-pages/discover-components/MainCarousel.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { HERO_GAMES } from "../../../dummy-server/DUMMY_GAMES";
+
 const MainCarousel = () => {
-    return <div></div>;
+    return (
+        <div className="main-carousel">
+            <div className="main-carousel__left">
+                <div>
+                    <img src={HERO_GAMES[0].posterBig} alt="poster" />
+                </div>
+            </div>
+            <div className="main-carousel__right"></div>
+        </div>
+    );
 };
 
 export default MainCarousel;
