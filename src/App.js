6d43afad754595ab83cf71dad37e018a902a0186
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-14 15:29:33 +0100

    rearranging components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import StorePage from "./components/store-page/StorePage";
+import Header from "./components/header/Header";
+
 function App() {
     return (
         <div>
+            <Header />
             <Routes>
                 <Route path="" element={<Navigate to="/store" />} />
-                <Route path="/store/*" element={<p>test</p>} />
+                <Route path="/store/*" element={<StorePage />} />
             </Routes>
         </div>
     );
