f87bfba6cf8ce5cb773f5261fd1f60889d962858
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-14 15:18:51 +0100

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import HomePage from "./components/home-page/HomePage";
-
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="" element={<Navigate to="/store" />} />
-                <Route path="/store/*" element={<HomePage />} />
+                <Route path="/store/*" element={<p>test</p>} />
             </Routes>
         </div>
     );
--- src/components/UI/PosterSmall.js
+++ src/components/UI/PosterSmall.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const PosterSmall = () => {
-    return <div></div>;
+const PosterSmall = ({ type, game, index }) => {
+    switch (type) {
+        case "hero":
+            return (
+                <div className="poster-small small-poster__size1">
+                    <div>
+                        <img src={game.posterSmall} alt="test" />
+                        <p>{game.name}</p>
+                    </div>
+                </div>
+            );
+
+        default:
+            return <p>TEST</p>;
+    }
 };
 
 export default PosterSmall;
