8ab3bd0baabe9f1d730274d26a1a3dd4a210861e
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-14 16:05:30 +0100

    added MainCarousel

--- src/components/store-page/StorePage.js
+++ src/components/store-page/StorePage.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import SubHeader from "../header/SubHeader";
+import Discover from "../sub-pages/Discover";
 
 const StorePage = () => {
     return (
         <div className="store-container">
             <SubHeader />
+
+            <Routes>
+                <Route path="" element={<Discover />} />
+            </Routes>
         </div>
     );
 };
