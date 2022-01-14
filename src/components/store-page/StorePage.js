3ab183ad3098b62f5322df7cfe30f6fb6cabbe06
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-14 15:57:53 +0100

    re work of the sub header completed
    still got to fix the look of the search form

--- src/components/store-page/StorePage.js
+++ src/components/store-page/StorePage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import SubHeader from "../header/SubHeader";
+
 const StorePage = () => {
-    return <div className="store-container"></div>;
+    return (
+        <div className="store-container">
+            <SubHeader />
+        </div>
+    );
 };
 
 export default StorePage;
