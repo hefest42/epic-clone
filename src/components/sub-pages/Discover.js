072b7c83617e4d01f1031213edbad9abbf1da762
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-17 16:29:56 +0100

    added freeGames

--- src/components/sub-pages/Discover.js
+++ src/components/sub-pages/Discover.js
@@ -3,6 +3,7 @@ import React, { Fragment } from "react";
 import MainCarousel from "./discover-components/MainCarousel";
 import GamesCarousel from "./discover-components/GamesCarousel";
 import FeaturedGames from "./discover-components/FeaturedGames";
+import FreeGames from "./discover-components/FreeGames";
 
 import { GAMES_ON_SALE, HERO_GAMES } from "../../dummy-server/DUMMY_GAMES";
 
@@ -12,6 +13,7 @@ const Discover = () => {
             <MainCarousel GAMES={HERO_GAMES} />
             <GamesCarousel title="Games On Sale" GAMES={GAMES_ON_SALE} />
             <FeaturedGames GAMES={HERO_GAMES} />
+            <FreeGames />
         </Fragment>
     );
 };
---
+++ src/components/sub-pages/discover-components/FreeGames.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FreeGames = () => {
+    return <div className="free-games"></div>;
+};
+
+export default FreeGames;
