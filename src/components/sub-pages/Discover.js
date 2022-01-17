34c8b8e542b2edbbdf90806f065d76af63409669
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-17 17:17:41 +0100

    init commit

--- src/components/sub-pages/Discover.js
+++ src/components/sub-pages/Discover.js
@@ -4,6 +4,7 @@ import MainCarousel from "./discover-components/MainCarousel";
 import GamesCarousel from "./discover-components/GamesCarousel";
 import FeaturedGames from "./discover-components/FeaturedGames";
 import FreeGames from "./discover-components/FreeGames";
+import NewTopComing from "./discover-components/NewTopComing";
 
 import { GAMES_ON_SALE, HERO_GAMES } from "../../dummy-server/DUMMY_GAMES";
 
@@ -14,6 +15,7 @@ const Discover = () => {
             <GamesCarousel title="Games On Sale" GAMES={GAMES_ON_SALE} />
             <FeaturedGames GAMES={HERO_GAMES} />
             <FreeGames GAMES={HERO_GAMES} />
+            <NewTopComing GAMES={GAMES_ON_SALE} />
         </Fragment>
     );
 };
---
+++ src/components/sub-pages/discover-components/NewTopComing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewTopComing = () => {
+    return <div></div>;
+};
+
+export default NewTopComing;
