3c1ae7692080e120c10e6f2007a80d49430e344d
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-19 15:57:48 +0100

    starting from the beginning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import StorePage from "./components/store-page/StorePage";
-import Header from "./components/header-footer/Header";
-
 function App() {
     return (
         <div>
-            <Header />
             <Routes>
                 <Route path="" element={<Navigate to="/store" />} />
                 <Route path="/store/*" element={<StorePage />} />
--- src/components/UI/BuyNowButton.js
+++
@@ -1,8 +0,0 @@
-//CSS IN POSTERS.scss
-import React from "react";
-
-const BuyNowButton = ({ text }) => {
-    return <button className="buy-button__white"> {text} </button>;
-};
-
-export default BuyNowButton;
