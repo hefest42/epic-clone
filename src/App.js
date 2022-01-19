6fe68c231acb3425e02b1b98585704b08b87c286
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-19 19:16:25 +0100

    left part of the header re worked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import StorePage from "./components/Pages/StorePage";
+
 function App() {
     return (
         <div>
             <Routes>
                 <Route path="" element={<Navigate to="/store" />} />
-                <Route path="/store/*" element={<p>start</p>} />
+                <Route path="/store/*" element={<StorePage />} />
             </Routes>
         </div>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-left">
+                <NavLink to="" className={navData => (navData.isActive ? "header-left__link link-active" : "header-left__link")}>
+                    <div className="header-left__link-top">
+                        <span>STORE</span>
+                    </div>
+                    <div className="header-left__link-down"></div>
+                </NavLink>
+
+                <NavLink to="faq" className={navData => (navData.isActive ? "header-left__link link-active" : "header-left__link")}>
+                    <div className="header-left__link-top">
+                        <span>FAQ</span>
+                    </div>
+                    <div className="header-left__link-down"></div>
+                </NavLink>
+
+                <NavLink to="help" className={navData => (navData.isActive ? "header-left__link link-active" : "header-left__link")}>
+                    <div className="header-left__link-top">
+                        <span>HELP</span>
+                    </div>
+                    <div className="header-left__link-down"></div>
+                </NavLink>
+
+                <NavLink to="unreal-engine" className={navData => (navData.isActive ? "header-left__link link-active" : "header-left__link")}>
+                    <div className="header-left__link-top">
+                        <span>UNREAL ENGINE</span>
+                    </div>
+                    <div className="header-left__link-down"></div>
+                </NavLink>
+            </div>
+            <div className="header-right"></div>
+        </header>
+    );
+};
+
+export default Header;
