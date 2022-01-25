5f7b488f77f7937d943c962d91e3698a272e9428
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-25 16:25:42 +0100

    finished with new, top, and coming game columns

--- src/components/Pages/store-page/StorePage.js
+++ src/components/Pages/store-page/StorePage.js
@@ -8,12 +8,16 @@ import HeroCarousel from "./HeroCarousel";
 import GameCarousel from "./GameCarousel";
 import FeaturedGames from "./FeaturedGames";
 import FreeGames from "./FreeGames";
+import NewTopComing from "./NewTopComing";
 
 import { GAMES } from "../../../dummy-server/DUMMY_GAMES";
 
 const StorePage = () => {
     const heroGames = GAMES.filter(game => game.gameFeatured === true);
     const gamesOnSale = GAMES.filter(game => game.gameOnSale === true);
+    const newGames = GAMES.slice(0, 5);
+    const topGames = GAMES.slice(5, 10);
+    const comingGames = GAMES.slice(2, 7);
 
     return (
         <Fragment>
@@ -35,6 +39,7 @@ const StorePage = () => {
                                 firstGame={GAMES.find(game => game.name === "Sifu")}
                                 secondGame={GAMES.find(game => game.name === "Rainbow Six: Extraction")}
                             />
+                            <NewTopComing newReleases={newGames} topSellers={topGames} comingSoon={comingGames} />
                         </div>
                     }
                 />
--- src/components/UI/HorizontalPoster.js
+++ src/components/UI/HorizontalPoster.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const HorizontalPoster = ({ game, showPrice }) => {
+const HorizontalPoster = ({ game, showPrice, index, posterNumber }) => {
     return (
         <div className="horizontal-poster">
             <div className="horizontal-poster__left">
                 <img src={game.posterSmall} alt="" />
+                {index === posterNumber && (
+                    <div className="poster-cover__white">
+                        <button>+</button>
+                    </div>
+                )}
             </div>
 
             <div className="horizontal-poster__right">
