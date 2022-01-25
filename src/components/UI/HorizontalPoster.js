0a049383b8288061c013ed1fcf705b09b04fda4f
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-25 17:20:33 +0100

    fixed the issue with showing games on sale

--- src/components/UI/HorizontalPoster.js
+++ src/components/UI/HorizontalPoster.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const HorizontalPoster = ({ game, showPrice, index, posterNumber }) => {
+    const calcDiscount = (price, discount) => {
+        return (price * (100 - discount)) / 100;
+    };
+
     return (
         <div className="horizontal-poster">
             <div className="horizontal-poster__left">
@@ -14,7 +18,14 @@ const HorizontalPoster = ({ game, showPrice, index, posterNumber }) => {
 
             <div className="horizontal-poster__right">
                 <span>{game.name}</span>
-                {showPrice && <span>{game.price}</span>}
+                {showPrice && game.gameOnSale && (
+                    <div className="game-on-sale">
+                        <span>-{game.discount}%</span>
+                        <span>${game.price}</span>
+                        <span>${calcDiscount(game.price, game.discount).toFixed(2)}</span>
+                    </div>
+                )}
+                {showPrice && !game.gameOnSale && <span>${game.price}</span>}
             </div>
         </div>
     );
