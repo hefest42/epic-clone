c4c018c139c8fcb58ae2b9491a49aaa3eb813657
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-25 15:28:54 +0100

    re-worked freeGames component

--- src/components/Pages/store-page/FreeGames.js
+++ src/components/Pages/store-page/FreeGames.js
@@ -1,34 +1,44 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 const FreeGames = ({ firstGame, secondGame }) => {
     return (
-        <section>
-            <div>
-                <div>Free Games</div>
+        <section className="free">
+            <div className="free-title">
+                <h3>Free Games</h3>
                 <button>VIEW MORE</button>
             </div>
 
-            <div>
-                <div>
-                    <div>
-                        <img src="" alt="" />
-                        <div>Free Now</div>
-                    </div>
-                    <div>
-                        <h5>Relicta</h5>
-                        <p>Free Now - Jan 27 at 05:00 PM</p>
-                    </div>
+            <div className="free-posters">
+                <div className="free-container">
+                    <Link to={`p/${firstGame.name.replace(":", "").split(" ").join("-").toLowerCase()}`}>
+                        <div className="free-image">
+                            <img src={firstGame.posterBig} alt="" />
+                            <div className="free-cover">
+                                <div className="free-cover__left">FREE NOW</div>
+                            </div>
+                        </div>
+                        <div className="free-info">
+                            <h4>Relicta</h4>
+                            <p>Free Now - Jan 27 at 05:00 PM</p>
+                        </div>
+                    </Link>
                 </div>
 
-                <div>
-                    <div>
-                        <img src="" alt="" />
-                        <div>COMING SOON</div>
-                    </div>
-                    <div>
-                        <h5>DAEMON X MACHINA</h5>
-                        <p>Free Jan 27 - Feb 03</p>
-                    </div>
+                <div className="free-container">
+                    <Link to={`p/${secondGame.name.replace(":", "").split(" ").join("-").toLowerCase()}`}>
+                        <div className="free-image">
+                            <img src={secondGame.posterBig} alt="" />
+                            <div className="free-cover">
+                                <div className="free-cover__right">COMING SOON</div>
+                            </div>
+                        </div>
+                        <div className="free-info">
+                            <h4>DAEMON X MACHINA</h4>
+                            <p>Free Jan 27 - Feb 03</p>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </section>
