8ace8153b62b60cb410c3899968c3b74db8c459f
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-27 14:26:02 +0100

    removed a import

--- src/components/Pages/store-page/GameCarousel.js
+++ src/components/Pages/store-page/GameCarousel.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -31,6 +31,8 @@ const GameCarousel = ({ title, games }) => {
         setPosterMovementAmount(state => state - 1);
         setPosterNumber(state => state - 6);
     };
+
+    console.log(posterMovementAmount, posterNumber);
 
     return (
         <section className="game-carousel">
