bfd9537315741afb9404eb33a82297b564cb3f7f
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-29 14:07:59 +0100

    changed the titles of the carousel items

--- src/components/Pages/browse-page/PopularGenresCarousel.js
+++ src/components/Pages/browse-page/PopularGenresCarousel.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import PopularGenresCarouselItem from "./PopularGenresCarouselItem";
 
+import { GAMES } from "../../../dummy-server/DUMMY_GAMES";
+
 const PopularGenresCarousel = () => {
     return (
         <section className="popular-genres">
@@ -14,7 +16,11 @@ const PopularGenresCarousel = () => {
             </div>
 
             <div className="popular-genres__container centered">
-                <PopularGenresCarouselItem />
+                <PopularGenresCarouselItem title="Action Games" games={GAMES.slice(0, 3)} index={0} />
+                <PopularGenresCarouselItem title="RPG Games" games={GAMES.slice(3, 6)} index={1} />
+                <PopularGenresCarouselItem title="Multiplayer Games" games={GAMES.slice(6, 9)} index={2} />
+                <PopularGenresCarouselItem title="Casual Games" games={GAMES.slice(9, 12)} index={3} />
+                <PopularGenresCarouselItem title="Horror Games" games={GAMES.slice(11, 14)} index={4} />
             </div>
         </section>
     );
--- src/components/Pages/browse-page/PopularGenresCarouselItem.js
+++ src/components/Pages/browse-page/PopularGenresCarouselItem.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { Link } from "react-router-dom";
 
-import { GAMES } from "../../../dummy-server/DUMMY_GAMES";
-
-const PopularGenresCarouselItem = () => {
+const PopularGenresCarouselItem = ({ title, games, index }) => {
     return (
-        <Link to="/store/p/god-of-war">
-            <div className="carousel-item centered-column">
-                <div className="carousel-item__images centered">
-                    <img className="carousel-item__images-1" src={GAMES[0].posterSmall} alt="" />
-                    <img className="carousel-item__images-2" src={GAMES[1].posterSmall} alt="" />
-                    <img className="carousel-item__images-3" src={GAMES[2].posterSmall} alt="" />
-                </div>
-                <div className="carousel-item__title centered">Action Games</div>
+        <Fragment>
+            <div className="carousel-item__wrapper" style={{ transform: `translateX(${index * 100}%)` }}>
+                <Link to="/store/p/god-of-war">
+                    <div className="carousel-item centered-column">
+                        <div className="carousel-item__images centered">
+                            <img className="carousel-item__images-1" src={games[0].posterSmall} alt="" />
+                            <img className="carousel-item__images-2" src={games[1].posterSmall} alt="" />
+                            <img className="carousel-item__images-3" src={games[2].posterSmall} alt="" />
+                        </div>
+                        <div className="carousel-item__title centered">{title}</div>
+                    </div>
+                </Link>
+                <div className="carousel-item__spacer"></div>
             </div>
-        </Link>
+        </Fragment>
     );
 };
 
 export default PopularGenresCarouselItem;
+
+// style={{ transform: `translateX(${index * 100}%)` }}
