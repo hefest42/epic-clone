e34400d339e09406dc859076bb38be2b4c3c1321
Nemanja Vidovic <hefest93@yahoo.com>
2022-01-31 18:17:02 +0100

    added an account slide

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import AccountSlice from "./AccountSlice";
+
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        account: AccountSlice,
+    },
 });
