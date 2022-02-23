f0ad966c0fda5200f268f9cbf7ec02244ca9c5ad
Nemanja Vidovic <hefest93@yahoo.com>
2022-02-23 14:00:01 +0100

    checkout information finished

--- src/components/Pages/wishlist-cart/Cart.js
+++ src/components/Pages/wishlist-cart/Cart.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const Cart = () => {
-    return <div></div>;
+    const navigate = useNavigate();
+    const isLoggedIn = useSelector(state => state.loggedInAccount.loggedIn);
+
+    return (
+        <div className="cart">
+            <div className="cart-title">My Cart</div>
+            <div className="cart-items centered">
+                <div className="cart-left"></div>
+
+                <div className="cart-right">
+                    <div className="cart-summary centered-column">
+                        <div className="cart-summary__title">Games and Apps Summary</div>
+                        <div className="cart-summary__top">
+                            <div className="cart-summary__price space-between">
+                                <p>Price</p>
+                                <p>$39.99</p>
+                            </div>
+                            <div className="cart-summary__taxes space-between">
+                                <p>Taxes</p>
+                                <p>Calculated at Checkout</p>
+                            </div>
+                        </div>
+                        <div className="cart-summary__bottom">
+                            <div className="cart-summary__total space-between">
+                                <p>Subtotal</p>
+                                <p>$50.99</p>
+                            </div>
+                            <button className="cart-summary__button">CHECK OUT</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Cart;
---
+++ src/components/Pages/wishlist-cart/CartItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartItem = () => {
+    return (
+        <div className="cartItem">
+            <div className="cartItem-Inner"></div>
+        </div>
+    );
+};
+
+export default CartItem;
