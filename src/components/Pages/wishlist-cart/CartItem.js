dbde8e28181885e071ffefe44ab451d0a2de8cdf
Nemanja Vidovic <hefest93@yahoo.com>
2022-02-24 19:05:29 +0100

    cartItem layout finished

--- src/components/Pages/wishlist-cart/CartItem.js
+++ src/components/Pages/wishlist-cart/CartItem.js
@@ -5,7 +5,19 @@ const CartItem = () => {
         <div className="cartItem centered">
             <div className="cartItem-inner centered">
                 <div className="cartItem-image"></div>
-                <div className="cartItem-information"></div>
+                <div className="cartItem-information">
+                    <div className="cartItem-information__name space-between">
+                        <div>Cyberpunk 2076</div>
+                        <div>$29.99</div>
+                    </div>
+                    <div className="cartItem-information__buttons space-between">
+                        <div></div>
+                        <div>
+                            <button>Move to wishlist</button>
+                            <button>Remove</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
