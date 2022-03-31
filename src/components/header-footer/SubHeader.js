5b89871c75d0f5cbfaacb16a9e5bdf72fff3e0d4
Nemanja Vidovic <hefest93@yahoo.com>
2022-03-31 15:54:19 +0200

    discover page search implemted

--- src/components/header-footer/SubHeader.js
+++ src/components/header-footer/SubHeader.js
@@ -1,18 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+import { GAMES } from "../../dummy-server/DUMMY_GAMES";
+
 const SubHeader = () => {
+    const navigate = useNavigate();
     const isLoggedIn = useSelector(state => state.loggedInAccount.loggedIn);
     const cartGames = useSelector(state => state.loggedInAccount.cart);
+    const [searchInput, setSearchInput] = useState("");
+    const [recommendedSearches, setRecommendedSearches] = useState([]);
+
+    const searchSubmitHandler = e => {
+        e.preventDefault();
+    };
+
+    const recommendedSearchHandler = string => {
+        const gamesByName = GAMES.map(game => game.name);
+        const allGenres = [
+            ...new Set(
+                GAMES.map(game => game.genres)
+                    .flat()
+                    .sort((a, b) => a.localeCompare(b))
+            ),
+        ];
+
+        if (allGenres.includes(string)) {
+            const index = allGenres.findIndex(str => str === string);
+
+            navigate(`/store/browse?genre${index}=${string}`);
+        }
+
+        if (gamesByName.includes(string)) {
+            navigate(`p/${string.replace(":", "").split(" ").join("-").toLowerCase()}`);
+        }
+
+        setSearchInput("");
+        setRecommendedSearches([]);
+    };
+
+    useEffect(() => {
+        if (searchInput === "") {
+            setRecommendedSearches([]);
+            return;
+        }
+        setRecommendedSearches([]);
+
+        const gamesByName = GAMES.map(game => game.name);
+        const allGenres = [...new Set(GAMES.map(game => game.genres).flat())];
+
+        const filterGamesByName = gamesByName.filter(game => game.toLowerCase().includes(searchInput.toLowerCase()));
+        const filterGenres = allGenres.filter(genre => genre.toLowerCase().includes(searchInput.toLowerCase()));
+
+        setRecommendedSearches(state => [...state, ...filterGamesByName.slice(0, 2), ...filterGenres.slice(0, 2)]);
+    }, [searchInput]);
 
     return (
         <div className="subheader">
             <div className="subheader-left">
-                <form action="">
+                <form action="" onSubmit={searchSubmitHandler}>
                     <span>&#128269;</span>
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search" value={searchInput} onChange={e => setSearchInput(e.target.value)} />
+
+                    {recommendedSearches.length > 0 && (
+                        <div className="subheader-recommended">
+                            {recommendedSearches.map((search, i) => (
+                                <p className="subheader-recommended__item" key={i} onClick={() => recommendedSearchHandler(search)}>
+                                    {search}
+                                </p>
+                            ))}
+                        </div>
+                    )}
                 </form>
 
                 <div>
